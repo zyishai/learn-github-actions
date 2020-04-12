7239807d66be93ea011f74d5d1e35fa4d2a2edc1
Yishai Zehavi <zehaviyishai@gmail.com>
2020-04-12 15:40:16 +0300

    added logic to find the release type #major

---
+++ scripts/find_last_tag_and_release.sh
@@ -0,0 +1,23 @@
+#! /bin/sh -l
+
+# fetch all tags
+git fetch --depth=1 origin +refs/tags/*:refs/tags/*
+
+# find the latest tag
+t=$(git describe --tags `git rev-list --tags --max-count=1`)
+
+if [ -z "$t" ]
+then
+    log=$(git log --pretty=oneline)
+    t=0.0.0
+else
+    log=$(git log $t..HEAD --pretty=oneline)
+fi
+
+case "$log" in
+    *#major* ) echo "::set-output name=release::major";;
+    *#patch* ) echo "::set-output name=release::patch";;
+    * ) echo "::set-output name=release::minor";;
+esac
+
+echo "::set-output name=last_tag::$t"
